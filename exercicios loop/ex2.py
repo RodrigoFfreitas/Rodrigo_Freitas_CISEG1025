997eb1253d45c12b97e6f423034b6f7a06f71db7
riguigo <contact.rodrigo.freitas@gmail.com>
2025-11-12 22:44:10 +0000

    ex2 loop

---
+++ exercicios loop/ex2.py
@@ -0,0 +1,7 @@
+
+for i in range(10):
+    num=int(input("Insira um numero inteiro: "))
+    if num % 2 == 0:
+        print("O numero inserido é par")
+    if num % 2 == 1:
+        print("O numero inserido é impar")
