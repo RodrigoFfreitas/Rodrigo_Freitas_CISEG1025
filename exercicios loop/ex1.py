310a6f8dafdc329260428622154fac239e610ce6
riguigo <contact.rodrigo.freitas@gmail.com>
2025-11-19 16:28:30 +0000

    edited ex1 and added ex5

--- exercicios loop/ex1.py
+++ exercicios loop/ex1.py
@@ -1,6 +1,13 @@
+npar=0
+nimpar=0
+i=0
 
-for i in range(30):
+while npar <= 30 and nimpar <= 30:
     if i % 2 == 0:
         print(f"O número {i} é par")
-    if i % 2 == 1:
+        npar += 1
+        i += 1
+    else:
         print(f"O número {i} é impar")
+        nimpar += 1
+        i += 1
